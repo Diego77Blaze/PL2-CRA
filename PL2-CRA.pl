75b5cd4cd9dd29f2b1bb591cfcaedc4c1ecc393e
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-03-26 11:44:34 +0100

    Create PL2-CRA.pl

---
+++ PL2-CRA.pl
@@ -0,0 +1,30 @@
+oracion --> g_nominal, g_verbal.
+g_nominal --> nombre.
+g_nominal --> determinante, nombre.
+g_nominal --> nombre, adjetivo.
+g_verbal --> verbo.
+g_verbal --> verbo, g_nominal.
+g_verbal --> verbo, adjetivo.
+
+%Diccionario
+determinante --> [el].
+determinante --> [la].
+determinante --> [un].
+determinante --> [una].
+nombre --> [hombre].
+nombre --> [mujer].
+nombre --> [juan].
+nombre --> [maria].
+nombre --> [manzana].
+nombre --> [gato].
+nombre --> [raton].
+nombre --> [alumno].
+nombre --> [universidad].
+verbo --> [ama].
+verbo --> [come].
+verbo --> [estudia].
+adjetivo --> [roja].
+adjetivo --> [negro].
+adjetivo --> [grande].
+adjetivo --> [gris].
+adjetivo --> [pequeno].
