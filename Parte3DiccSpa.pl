e77773fc72d59f8b839acfc7883dfca0140a87ea
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-04-02 15:46:44 +0200

    Update Parte3DiccSpa.pl

--- Parte3DiccSpa.pl
+++ Parte3DiccSpa.pl
@@ -6,11 +6,11 @@ determinante(det(art),m,sg,und) --> [un].
 determinante(det(art),f,sg,und) --> [una].
 determinante(det(art),m,pl,und) --> [unos].
 determinante(det(art),f,pl,und) --> [unas].
-determinante(det(pos),s) --> [mi].%no se si vale así
+determinante(det(pos),s) --> [mi].
 
 
-pronombre(pron(pron), m, terc, pl, pers) --> [ellos].%no hace falta genero?
-pronombre(pron(pron), m, seg, sg, pers) --> [tu].
+pronombre(pron(pron), m, terc, pl, pers, m) --> [ellos].
+pronombre(pron(pron), m, seg, sg, pers, _) --> [tu].
 
 
 nombre(n(n_1),f,sg,com) --> [piedra].
@@ -32,27 +32,27 @@ nombre(n(n_15),m,sg,com) --> [cafe].
 nombre(n(n_16),m,sg,com) --> [periodico].
 nombre(n(n_17),m,sg,prop) --> [oscar].
 nombre(n(n_18),m,sg,prop) --> [wilde].
-nombre(n(n_19),m,sg,com) --> [fantasma].%problema con esto
+nombre(n(n_19),m,sg,com) --> [fantasma].
 
 
-verbo(v(v_1),terc,sg) --> [corta].
-verbo(v(v_1),terc,pl) --> [cortan].
-verbo(v(v_2),terc,sg) --> [envuelve].
-verbo(v(v_2),terc,pl) --> [envuelven].
-verbo(v(v_3),terc,sg) --> [rompe].
-verbo(v(v_3),terc,pl) --> [rompen].
-verbo(v(v_4),seg, sg) --> [comes].
-verbo(v(v_4),terc, sg) --> [come].
-verbo(v(v_4),terc, pl) --> [comen].
-verbo(v(v_5),terc, sg) --> [ama].
-verbo(v(v_6),terc, sg) --> [estudia].
-verbo(v(v_7),terc, sg) --> [persiguio].%insertar tiempo?
-verbo(v(v_8),terc, sg) --> [es].
-verbo(v(v_9),prim, pl) --> [vimos].
-verbo(v(v_10),terc, sg) --> [canta].
-verbo(v(v_11),terc, sg) --> [toma].
-verbo(v(v_12),terc, sg) --> [lee].
-verbo(v(v_13),terc, sg) --> [escribio].%insertar tiempo?
+verbo(v(v_1),terc,sg,pres) --> [corta].
+verbo(v(v_1),terc,pl,pres) --> [cortan].
+verbo(v(v_2),terc,sg,pres) --> [envuelve].
+verbo(v(v_2),terc,pl,pres) --> [envuelven].
+verbo(v(v_3),terc,sg,pres) --> [rompe].
+verbo(v(v_3),terc,pl,pres) --> [rompen].
+verbo(v(v_4),seg, sg,pres) --> [comes].
+verbo(v(v_4),terc, sg,pres) --> [come].
+verbo(v(v_4),terc, pl,pres) --> [comen].
+verbo(v(v_5),terc, sg,pres) --> [ama].
+verbo(v(v_6),terc, sg,pres) --> [estudia].
+verbo(v(v_7),terc, sg,pas) --> [persiguio].%insertar tiempo?
+verbo(v(v_8),terc, sg,pres) --> [es].
+verbo(v(v_9),prim, pl,pas) --> [vimos].
+verbo(v(v_10),terc, sg,pres) --> [canta].
+verbo(v(v_11),terc, sg,pres) --> [toma].
+verbo(v(v_12),terc, sg,pres) --> [lee].
+verbo(v(v_13),terc, sg,pas) --> [escribio].
 
 adjetivo(adj(adj_1), f, sg) --> [roja].
 adjetivo(adj(adj_2), m, sg) --> [grande].
@@ -68,7 +68,7 @@ preposicion(prep(prep_3)) --> [de].
 
 nexo(nx(nx_1)) --> [que].
 
-adverbio(adv(adv_1)) --> [ayer].%añadir tiempo lugar?
+adverbio(adv(adv_1)) --> [ayer].
 adverbio(adv(adv_2)) --> [bien].
 
 cuantificador(cuant(cuant_1)) --> [muy].
