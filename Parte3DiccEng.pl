6860076305d6821d8c99960e4cb3dd449a88655e
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-04-10 15:06:59 +0200

    Revert "Content Updated"
    This reverts commit ee9701a90b3b874d2c66dac809a1fe79f92fe462.

--- Parte3DiccEng.pl
+++ Parte3DiccEng.pl
@@ -1,13 +1,12 @@
-﻿%determinante
-determinant(det(art_1),_,_,def,_) --> [the].
+﻿determinant(det(art_1),_,_,def,_) --> [the].
 determinant(det(art_1),_,_,und,_) --> [].
 determinant(det(art_2),_,sg,und,nv) --> [a].
 determinant(det(art_2),_,sg,und,v) --> [an].
 determinant(det(art_3),_,sg,und,_) --> [].
 determinant(det(art_4),_,pl,und,_) --> [some].
 determinant(det(pos),_,sg,pos,_) --> [my].
-determinant(det(pos),_,_,_,_) --> [].
-%pronombre
+determinant(det(pos),_,_,_,_) --> [].%
+
 pronoun(pron(pron_1),prim,sg,_) --> [i].
 pronoun(pron(pron_2),seg,sg,_) --> [you].
 pronoun(pron(pron_3),terc,sg,m) --> [he].
@@ -15,7 +14,7 @@ pronoun(pron(pron_4),terc,sg,f) --> [she].
 pronoun(pron(pron_5),prim,pl,_) --> [we].
 pronoun(pron(pron_6),seg,pl,_) --> [you].
 pronoun(pron(pron_7),terc,pl,_) --> [they].
-%nombre
+
 noun(n(n_1),_,sg,com,nv,terc) --> [rock].
 noun(n(n_2),_,sg,com,nv,terc) --> [paper].
 noun(n(n_3),_,pl,com,nv,terc) --> [scissors].
@@ -23,7 +22,7 @@ noun(n(n_4),_,sg,com,v,terc) --> [apple].
 noun(n(n_5),m,sg,com,nv,terc) --> [man].
 noun(n(n_6),m,_,prop,nv,terc) --> [john].
 noun(n(n_7),f,sg,prop,nv,terc) --> [mary].
-noun(n(n_8),_,sg,com,nv,terc) --> [cat].
+noun(n(n_8),_,sg,com,nv,terc) --> [cat].%añadir atributo big p.ej
 noun(n(n_9),_,sg,com,nv,terc) --> [mouse].
 noun(n(n_10),_,sg,com,v,terc) --> [university].
 noun(n(n_11),_,sg,com,nv,terc) --> [student].
@@ -39,7 +38,7 @@ noun(n(n_21),m,sg,com,nv,terc) --> [garden].
 noun(n(n_22),_,_,com,nv,terc) --> [canterville, ghost].
 noun(n(n_23),_,pl,com,v,terc) --> [apples].
 noun(n(n_24),_,sg,com,nv,terc) --> [mice].
-%verbo
+
 verb(vb(v_1),terc,sg,pres) --> [cuts].
 verb(vb(v_1),prim,_,_) --> [cut].
 verb(vb(v_1),seg,_,_) --> [cut].
@@ -71,7 +70,7 @@ verb(vb(v_15),terc,sg,pas) --> [was].
 verb(vb(v_15),prim,_,pas) --> [were].
 verb(vb(v_15),seg,_,pas) --> [were].
 verb(vb(v_15),terc,pl,pas) --> [were].
-%adjetivo
+
 adjective(adj(adj_1),_,_,nv) --> [red].
 adjective(adj(adj_2),_,_,nv) --> [big].
 adjective(adj(adj_3),_,_,nv) --> [gray].
@@ -79,22 +78,21 @@ adjective(adj(adj_4),_,_,nv) --> [black].
 adjective(adj(adj_5),_,_,nv) --> [yellow].
 adjective(adj(adj_6),_,_,nv) --> [thin].
 adjective(adj(adj_7),_,_,nv) --> [tall].
-adverbio(adv(adv_6)) --> [no].
-%preposicion
+
 preposition(prep(prep_1)) --> [at].
 preposition(prep(prep_2)) --> [].
-%nexo
+
 nexus(nx(nx_1)) --> [that].
-%adverbio
+
 adverb(adv(adv_1)) --> [yesterday].
 adverb(adv(adv_2)) --> [well].
 adverb(adv(adv_3)) --> [today].
 adverb(adv(adv_4)) --> [there].
 adverb(adv(adv_5)) --> [here].
 adverb(adv(adv_6)) --> [no].
-%cuantificador
+
 quantifier(cuant(cuant_1)) --> [very].
-%conjuncion
+
 conjunction(conj(conj_1)) --> [and].
 conjunction(conj(conj_2)) --> [or].
 conjunction(conj(conj_3)) --> [or].
